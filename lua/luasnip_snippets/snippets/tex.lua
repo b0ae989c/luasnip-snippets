338832a446edc1fb976a9df787d43fccdafe4f07
Yifan Hu <9336254-b0ae989c@users.noreply.gitlab.com>
2023-08-03 20:28:51 -0500

    feat: add tex snippets

--- lua/luasnip_snippets/snippets/tex.lua
+++ lua/luasnip_snippets/snippets/tex.lua
@@ -1,25 +1,61 @@
-local snippets = {
-  s(
+local s_begin = s(
+  {
+    trig = 'beg',
+    name = 'begin … end',
+  },
+  fmt(
+    [[
+    \begin{<1>}
+    	<2>
+    \end{<1>}
+    ]],
     {
-      trig = 'beg',
-      name = '\\begin{} … \\end{}',
+      i(1, ''),
+      i(2, ''),
     },
-    fmt(
-      [[
-  \begin{<1>}
-    <2>
-  \end{<1>}
-  ]],
-      {
-        i(1, ''),
-        i(2, ''),
-      },
-      {
-        delimiters = '<>',
-        repeat_duplicates = true,
-      }
-    )
-  ),
+    {
+      delimiters = '<>',
+      repeat_duplicates = true,
+    }
+  )
+)
+
+local s_ali = s(
+  {
+    trig = 'ali',
+    name = 'align',
+  },
+  fmt(
+    [[
+    \begin{aligned}
+    	<1>
+    \end{aligned}
+    ]],
+    { i(1, '') },
+    { delimiters = '<>' }
+  )
+)
+
+local s_eqn = s(
+  {
+    trig = 'eqn',
+    name = 'equation',
+  },
+  fmt(
+    [[
+    \begin{equation}
+    	<1>
+    \end{equation}
+    ]],
+    { i(1, '') },
+    { delimiters = '<>' }
+  )
+)
+
+local snippets = {
+  s_begin,
+  s_ali,
+  s_eqn,
 }
 
 local autosnippets = {}
