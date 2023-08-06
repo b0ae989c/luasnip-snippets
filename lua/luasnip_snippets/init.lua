0e0d1ccdff431ba77b70d5309f653efb8383f207
Yifan Hu <9336254-b0ae989c@users.noreply.gitlab.com>
2023-08-05 22:51:29 -0500

    fix: use lazy_load

--- lua/luasnip_snippets/init.lua
+++ lua/luasnip_snippets/init.lua
@@ -5,7 +5,7 @@ local M = {}
 function M.load(option)
 	config.setup(option or {})
 
-	require('luasnip.loaders.from_lua').load({
+	require('luasnip.loaders.from_lua').lazy_load({
 		paths = vim.api.nvim_get_runtime_file(
 			'lua/luasnip_snippets/snippets/',
 			false
