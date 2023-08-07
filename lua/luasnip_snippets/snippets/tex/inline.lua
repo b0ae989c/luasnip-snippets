5d8563b872c159d5b96de9b27223a2d0f64cdd73
Yifan Hu <9336254-b0ae989c@users.noreply.gitlab.com>
2023-08-07 18:12:30 -0500

    feat: add tex snippets

--- lua/luasnip_snippets/snippets/tex/inline.lua
+++ lua/luasnip_snippets/snippets/tex/inline.lua
@@ -39,6 +39,41 @@ local snippets = {
 		name = 'fraction',
 		dscr = '',
 	}, fmta([[{\frac{<1>}{<2>}}]], { i(1, ''), i(2, '') })),
+	s({
+		trig = 'pd',
+		name = 'partial derivative',
+		dscr = '',
+	}, fmta([[{\frac{\partial}{\partial <1>}}]], { i(1, '') })),
+	s({
+		trig = 'lim',
+		name = 'limit',
+		dscr = '',
+	}, fmta([[\lim_{<1>}]], { i(1, '') })),
+	s({
+		trig = 'sum',
+		name = 'summation',
+		dscr = '',
+	}, fmta([[\sum_{<1>}^{<2>}]], { i(1, ''), i(2, '') })),
+	s({
+		trig = 'prod',
+		name = 'product',
+		dscr = '',
+	}, fmta([[\prod_{<1>}^{<2>}]], { i(1, ''), i(2, '') })),
+	s({
+		trig = 'int',
+		name = 'integral',
+		dscr = '',
+	}, fmta([[\int_{<1>}^{<2>}]], { i(1, ''), i(2, '') })),
+	s({
+		trig = 'iint',
+		name = 'double integral',
+		dscr = '',
+	}, fmta([[\iint_{<1>}^{<2>}]], { i(1, ''), i(2, '') })),
+	s({
+		trig = 'oint',
+		name = 'line integral',
+		dscr = '',
+	}, fmta([[\oint_{<1>}^{<2>}]], { i(1, ''), i(2, '') })),
 }
 
 local autosnippets = {}
